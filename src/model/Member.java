f5f8a8e3a906d761481076013758b659d8b9de2a
Vahe Antonyan <vaheant@gmail.com>
2019-12-27 00:34:47 +0400

    Whole project

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,9 +1,5 @@
 package model;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
 public class Member {
 
     private String name;
@@ -11,29 +7,12 @@ public class Member {
     public Member() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
-    public void driver() {
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            System.out.println();
-            System.out.println(name);
-            System.out.println("Press Number for Action\n1. Send message\n2. Exit chat");
-            String st = scan.nextLine();
-            if (st.equals("1")) {
-                SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-                Date date = new Date(System.currentTimeMillis());
-                System.out.println(formatter.format(date) + " " + this.name + " " + "name: text");
-            } else {
-                if (st.equals("2")) {
-                    System.out.println("Member " + this.name + " left chat.");
-                    break;
-                } else {
-                    System.out.println("Not a valid input. Please choose one of the options below.");
-                }
-            }
-        }
+    public Member name(String name){
+        this.name = name;
+        return this;
     }
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,39 +1,9 @@
 package test;
 
-import model.Member;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+        import model.Chat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Input count of members:");
-        Scanner sc = new Scanner(System.in);
-        int membersCount = sc.nextInt();
-        Member[] membersArray = new Member[membersCount];
-        for (int i = 0; i < membersCount; ++i) {
-            membersArray[i] = new Member();
-            membersArray[i].setName("member" + (i + 1));
-        }
-
-        List<Member> activeMembersList = new ArrayList<Member>();
-        System.out.print("activeMembers: ");
-        for (int i = 0; i < membersCount; ++i) {
-            if (Math.random() >= 0.5) {
-                activeMembersList.add(membersArray[i]);
-                System.out.print(i+1 + " ");
-            }
-        }
-        int amlSize = activeMembersList.size();
-        System.out.println();
-        System.out.println("activeMembersList size: " + amlSize);
-
-        while (amlSize != 0) {
-            int index = (int)(Math.random() * amlSize);
-            activeMembersList.get(index).driver();
-            activeMembersList.remove(index);
-            --amlSize;
-        }
+        Chat chat = new Chat();
     }
 }
